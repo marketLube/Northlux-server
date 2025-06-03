4291923788b92a1b950b7c89b4a0d514224e7eed
Market lube <marketlubes@gmail.com>
2025-06-03 12:59:43 +0530

    bug 58

--- routes/v1/brandRoutes.js
+++ routes/v1/brandRoutes.js
@@ -13,14 +13,14 @@ const brandRouter = require("express").Router();
 
 brandRouter
   .route("/")
-  .post(autheticateToken(["admin", "seller"]), upload.any(), createBrand) // Create a new brand
+  .post(autheticateToken(["admin", "store"]), upload.any(), createBrand) // Create a new brand
   .get(getAllBrands); // Get all brands 
 
 brandRouter.route("/search").get(searchBrand);
 brandRouter
   .route("/:id")
   .get(getBrandById) // Get a brand by ID
-  .patch(autheticateToken(["admin", "seller"]), upload.any(), updateBrand) // Update a brand by ID
-  .delete(autheticateToken(["admin", "seller"]), deleteBrand); // Delete a brand by ID
+  .patch(autheticateToken(["admin", "store"]), upload.any(), updateBrand) // Update a brand by ID
+  .delete(autheticateToken(["admin", "store"]), deleteBrand); // Delete a brand by ID
 
 module.exports = brandRouter;
